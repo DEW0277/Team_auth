ec179aeef76916f6ead86d13067b0e83f446aae5
Dew0277 <jaloliddina98@gmail.com>
2024-11-16 17:42:22 +0500

    Create new Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500">Boshlaymiz...</h1>
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
