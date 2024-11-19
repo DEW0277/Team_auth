bafb141aa83d971ff34ef32e66c19df5ca7855f5
hamidullox27 <jasonman272727@gmail.com>
2024-11-19 17:20:20 +0500

    added hamidullox pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Men from "./pages/Men";
+import Hamidullox from "./pages/hamidullox";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1 className="text-red-500">Boshlaymiz...</h1>
       <Routes>
         <Route path="/" element={<Men />} />
+        <Route path="/hamidullox" element={<Hamidullox />} />
       </Routes>
     </>
   );
