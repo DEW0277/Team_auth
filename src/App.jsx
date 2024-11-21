c63fe131bafcd86c85a8ddd0b85faf27672ae570
Dew0277 <jaloliddina98@gmail.com>
2024-11-21 17:15:56 +0500

    men.png added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Men from "./pages/Men";
+import Mohinur from "./pages/mohinur";
 import Navbar from "./components/shared/Navbar";
 import Hamidullox from "./pages/hamidullox";
 
@@ -10,8 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Men />} />
+        <Route path="mohinur" element={<Mohinur />} />
         <Route path="hamidullox" element={<Hamidullox />} />
-
       </Routes>
     </>
   );
